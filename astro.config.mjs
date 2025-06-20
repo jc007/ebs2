e036bec3178217f089e50851ba069116ec72c1b9
Joh Chan <joh@untitleddesign.co.nz>
2025-06-20 12:41:51 +1200

    Add Github Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,7 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://jc007.github.io',
+  base: '/ebs2',
+});
