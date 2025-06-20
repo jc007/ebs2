0b08bbd12f99ecda774f5f11a85136a8b6b986d3
jc007 <joh@untitleddesign.co.nz>
2025-06-20 14:46:46 +1200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://jc007.github.io',
-  base: '/ebs2',
   outDir: 'publish'
 });
